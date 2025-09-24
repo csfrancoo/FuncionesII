63ae7e973eccbcfb110890fa47611701c98e424d
csfrancoo <cayofranco9@gmail.com>
2025-09-24 14:20:32 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+//ejercicios del practico
+console. log("=============")
+console. log("Practico 06 - funciones II")
+console. log("==============")
+ 
+//1. Funcion nativo de JavaScript 
+function saludar(){
+    console. log("Buenas Tardes, soy la funcion saludar")
+}
+ 
+// para ejecutar la funcion debemos llamarla 
+saludar()
+
+//funciones para login de usuario
+function. login(){
+    //1.Captura datos ddel formulario
+    let email = document.getElementById("email").value;
+    //2.captura los datos del password
+    let password = document.getElementById("password").value;
+    //3
+}
